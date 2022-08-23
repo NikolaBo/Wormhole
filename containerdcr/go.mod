4f7ffea4ecaf086543a30f36824eecc01a33027d
t-nbojanic <t-nbojanic@microsoft.com>
2022-08-23 01:42:12 +0000

    automate remote checkpoint

--- containerdcr/go.mod
+++ containerdcr/go.mod
@@ -27,6 +27,7 @@ require (
 	github.com/opencontainers/runc v1.1.2 // indirect
 	github.com/opencontainers/runtime-spec v1.0.3-0.20210326190908-1c3f411f0417 // indirect
 	github.com/opencontainers/selinux v1.10.1 // indirect
+	github.com/pelletier/go-toml v1.9.3 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
 	go.opencensus.io v0.23.0 // indirect
--- containerdcr/main.go
+++ containerdcr/main.go
@@ -5,21 +5,97 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"syscall"
 
 	"github.com/containerd/containerd"
 	"github.com/containerd/containerd/cio"
 	"github.com/containerd/containerd/namespaces"
+	"github.com/containerd/containerd/remotes"
+	"github.com/containerd/containerd/remotes/docker"
+	"github.com/containerd/containerd/remotes/docker/config"
 )
 
+var client *containerd.Client
+var ctx context.Context
+
 func main() {
-	if err := webAppExample(); err != nil {
+	fmt.Printf("Attempting to open containerd client connection...\n")
+	var err error
+	client, err = containerd.New("/run/containerd/containerd.sock")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
+	fmt.Printf("Successfully opened containerd client connection!\n")
+
+	ctx = namespaces.WithNamespace(context.Background(), "k8s.io")
+
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello!\n")
+		fmt.Printf("/hello endpoint accessed\n")
+	})
+
+	http.HandleFunc("/checkpoint", createCheckpoint)
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func webAppExample() error {
+func createCheckpoint(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("/checkpoint endpoint accessed\n")
+
+	ids, ok := r.URL.Query()["id"]
+	if !ok || len(ids[0]) < 1 {
+		fmt.Fprintf(w, "Url Param 'id' is missing\n")
+		return
+	}
+	containerId := ids[0]
+
+	container, err := client.LoadContainer(ctx, containerId)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Checkpointing container %s...\n", containerId)
+	imageStore := client.ImageService()
+	checkpoint, err := container.Checkpoint(ctx, "wrmcheckpt", []containerd.CheckpointOpts{
+		containerd.WithCheckpointRuntime,
+		containerd.WithCheckpointRW,
+		containerd.WithCheckpointTask,
+	}...)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer imageStore.Delete(ctx, checkpoint.Name())
+	fmt.Printf("Checkpoint created\n")
+
+	resolver := GetResolver()
+	ropts := []containerd.RemoteOpt{containerd.WithResolver(resolver)}
+
+	err = client.Push(ctx, "docker.io/nikolabo/io-checkpoint:latest", checkpoint.Target(), ropts...)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func GetResolver() remotes.Resolver {
+	options := docker.ResolverOptions{}
+	user := ""
+	access := ""
+	hostOptions := config.HostOptions{}
+	hostOptions.Credentials = func(host string) (string, string, error) {
+		return user, access, nil
+	}
+
+	options.Hosts = config.ConfigureHosts(ctx, hostOptions)
+	return docker.NewResolver(options)
+}
+
+func restoreFromCheckpoint() error {
 	client, err := containerd.New("/run/containerd/containerd.sock")
 	if err != nil {
 		return err
